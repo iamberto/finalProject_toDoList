aa1666a1389dd71811ed4c1004399a78b12a48e6
berto <asandoval.hatchet@gmail.com>
2019-09-18 00:38:49 -0700

    Adding todo functionality

--- js/main.js
+++ js/main.js
@@ -1,62 +1,27 @@
-function playWithApi(){
-    const url = "https://dog.ceo/api/breed/shihtzu/images/random"
-    fetch(url);
-    // console.log(url);
+//Declare variables from DOM
+let input = document.querySelector("input [type='text']");
+let ul = document.querySelector("ul");
 
-    const exampleTwo='{"random":7, "phrase":"This is super cool", "name":"Berto"}';
-    // console.log(exampleTwo);
+//Event listener for input and making new todo Items
 
-    const exampleParse=JSON.parse(exampleTwo);
-    // console.log(exampleParse.name);
+input.addEventListener("keypress", function(keypressed){
+    if(keypressed.which === 13){
+        //Add elements after you hit enter
+        let li = document.createElement('li');
+        let spanElement = document.createElement('span');
+        let icon = document.createElement('i');
 
-    const exampleStringify=JSON.stringify(exampleTwo);
-    // console.log(exampleStringify);
+        let newTodo = this.value;
+        this.value = ""; 
 
+        icon.classList.add('fas','fa-trash-alt');
 
-}
+        //Append items
+        spanElement.append(icon);
+        ul.appendChild(li).append(spanElement, newTodo);
 
-// function practiceScope(){
-//     let random = 'Inside scope';
-//     const phrase = function(){
-//         console.log(random);
-//     }
-//     return phrase();
-// }
-// let practiceOutside = practiceScope;
-
-// practiceOutside();
-
-
-class AboutMe{
-    constructor(name, age, petName, petBreed) {
-        this.name = name;
-        this.age = age;
-        this.petName = petName;
-        this.petBreed = petBreed;
+        deleteTodo();
     }
 }
 
-
-const berto = new AboutMe('Berto', 28, 'Mushu', 'shihtzu');
-
-
-// make a new class that works with your api
-
-class AboutDawg{
-    constructor(name, age, size, breed) {
-        this.name;
-        this.age;
-        this.size;
-        this.breed;
-    }
-}
-
-AboutDawg.prototype.moosh = function(){
-    const url = `https://dog.ceo/api/breed/${this.breed}/images/random`;
-    fetch(url);
-    .then((transformToJson)=>
-        return transformToJson.json();
-})
-.then((breedData)=>{
-    console.log(breedData);
-})
+);
